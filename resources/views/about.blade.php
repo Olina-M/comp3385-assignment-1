18f556ae4f3e0bd417975cb8115b0ee79c9137f7
Olina Marie <olina.marie@my.fiveislands.uwi.edu>
2024-02-12 20:51:04 -0800

    Created a new layout, controller and view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app') 
+
+@section('content') 
+ <h2>Olina Wilisa Anique Marie</h2> 
+ <p>As previously mentioned, my name is known. I am a second-year SOSCAI student pursuing a degree at 
+    UWI Five Islands in the School of Science, Computing & Artificial Intelligence. At 22 years old, 
+    I aspire to forge a career in Information Technology within the medical field preferbly. As a female, 
+    my ambitions are centered around making a meaningful impact.
+ </p> 
+@endsection 
