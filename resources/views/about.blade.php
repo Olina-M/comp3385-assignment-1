ae459d4231147aca15a27d20a781d2b93f083c05
Olina Marie <olina.marie@my.fiveislands.uwi.edu>
2024-02-12 22:32:16 -0800

    Styled my page and added an image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app') 
 
 @section('content') 
+
  <h2>Olina Wilisa Anique Marie</h2> 
  <p>As previously mentioned, my name is known. I am a second-year SOSCAI student pursuing a degree at 
     UWI Five Islands in the School of Science, Computing & Artificial Intelligence. At 22 years old, 
     I aspire to forge a career in Information Technology within the medical field preferbly. As a female, 
     my ambitions are centered around making a meaningful impact.
  </p> 
+ <img src="{{ url('/images/olina.jpg') }}" alt="This is a picture of myself" style="width: 200px; height: auto;"/> 
 @endsection 
